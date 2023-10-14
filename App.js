4bc063179ed86de00ce28f59b1bbbb76a833457b
jun447 <junaidmumtaz16@gmail.com>
2023-10-14 21:53:06 +0500

    Added Another shape

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native
 export default function App() {
 
   const [color,setColor] = useState("#000000");
+  const [colorsq,setColorsq] = useState("#000000");
   
   const generateRandomColor = () => {
     console.log("clicked");
@@ -13,16 +14,30 @@ export default function App() {
     });
     setColor(randomColor);
   }
- 
+  const generateRandomColorSQ = () => {
+    console.log("clickedSQ");
+    let randomColor = "#000000".replace(/0/g,function(){
+      return (~~(Math.random()*16)).toString(16);
+    });
+    setColorsq(randomColor);
+  }
 
   return (
 
     <>
     <StatusBar backgroundColor={color} />
      <View style={[styles.container,{backgroundColor:color}]}>
+      <View style={[styles.box,{backgroundColor:colorsq}]} >
+
+      </View>
       <TouchableOpacity onPress={generateRandomColor}>
          <View style={styles.actionBtn}  >
             <Text>Click Me</Text>
+         </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={generateRandomColorSQ}>
+         <View style={styles.actionBtn}  >
+            <Text>Click Me Sq</Text>
          </View>
       </TouchableOpacity>
        <Text>Open up App.js to start working on your app!</Text>
@@ -33,12 +48,21 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  box:{
+    width: 200,
+    height: 200,
+    borderRadius: 12,
+    marginBottom: 10,
+    borderWidth: 2,
+    borderColor: "#FFFFFF"
+  },
   container:{
     flex:1,
     alignItems: "center",
     justifyContent: "center"
   },
   actionBtn: {
+    margin:10,
     borderRadius: 12,
     backgroundColor: "orange",
     paddingVertical: 10,
