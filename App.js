d4a74263fc251c8be0739770daa7fc59025ea67a
jun447 <junaidmumtaz16@gmail.com>
2023-10-14 21:36:13 +0500

    Added Function to generate random color

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 // import {  } from 'expo-status-bar';
+import { useState } from 'react';
 import {StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [color,setColor] = useState("#000000");
+  
+  const generateRandomColor = () => {
+    console.log("clicked");
+    let randomColor = "#000000".replace(/0/g,function(){
+      return (~~(Math.random()*16)).toString(16);
+    });
+    setColor(randomColor);
+  }
+ 
+
   return (
 
     <>
-    <StatusBar backgroundColor={"#000000"} />
-     <View style={[styles.container]}>
-      <TouchableOpacity>
+    <StatusBar backgroundColor={color} />
+     <View style={[styles.container,{backgroundColor:color}]}>
+      <TouchableOpacity onPress={generateRandomColor}>
          <View style={styles.actionBtn}  >
             <Text>Click Me</Text>
          </View>
@@ -27,9 +40,14 @@ const styles = StyleSheet.create({
   },
   actionBtn: {
     borderRadius: 12,
-    backgroundColor: "#6A1B4D",
+    backgroundColor: "orange",
     paddingVertical: 10,
-    paddingHorizontal: 40
+    paddingHorizontal: 40,
+    transition: "all 0.3s ease-in-out",
+    "&:hover": {
+      backgroundColor: "#8B5F9E",
+      transform: "scale(1.1)"
+    }
   },
   actionBtnTxt: {
     fontSize: 24,
